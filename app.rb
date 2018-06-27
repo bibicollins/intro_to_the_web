f54ec62d2de3a737a9b5f00a63f0323192cea2bf
bibi collins <bibicollins94@googlemail.com>
2018-06-27 14:27:46 +0100

    Work from day 2- making a @name variable to keep views clean, added params & adding a form to /named-cat

--- app.rb
+++ app.rb
@@ -8,9 +8,14 @@ end
 get '/bikes' do
   "I am Elishka's spirit animal |<8"
 end
-get '/cat' do
-  "<div style='border: 3px dashed red'>
-  <img src=http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png>
-  </div>"
 
+get '/random-cat' do
+  @name = ["Bibi", "Elishka", "Bike", "Frog"].sample
+  erb(:index)
 end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
+end 
